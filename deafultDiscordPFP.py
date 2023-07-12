0cbfe0465c3ae9354982f2afb42c999531d8943a
bigmamcallum <47284263+bigmancallum@users.noreply.github.com>
2023-07-12 15:20:00 +0100

    Legacy and current system work.

--- deafultDiscordPFP.py
+++ deafultDiscordPFP.py
@@ -1,13 +1,54 @@
-def menu():
-    option = input("""
-    Is the user using:
-    [1] The legacy username system
-    [2] 
-    """)
+from urllib.request import Request, urlopen #For opening online images
+from tkinter import * #For pop-up
+from PIL import ImageTk #For image within tk pop-up
 
-userID = int(input("User ID: "))
+option = int(input("""
+Is the user using:
+[1] The legacy username system
+[2] The current username system
+[3] I don't know
+                   
+Enter option: """))
 
-shiftedID = userID >> 22 #Right binary shift userID by 22 places
-modID = shiftedID % 6 #shiftedID modulo 6
+if option == 1: #Legacy system
+    desc = int(input("Please enter the user's descriminator (the four numbers after the # in their username): "))
+    modDesc = desc % 5 #desc modulo 5
+    print (f"The profile picture can be seen at: https://cdn.discordapp.com/embed/avatars/{modDesc}.png")
+    showImage = ""
+    while showImage.capitalize() not in ["Y", "Yes", "N", "No"]:
+        showImage = input("Would you like to see the image? [Y]es or [N]o: ")
+    if showImage.capitalize() in ["Y", "Yes"]:
+        req = Request(
+            url=f'https://cdn.discordapp.com/embed/avatars/{modDesc}.png', #Image URL
+            headers={'User-Agent': 'Mozilla/5.0'} #Tells Discord CDN we are using a Mozilla browser
+        )
+        #Gets the image from the URL and displays it
+        root = Tk()
+        data = urlopen(req)
+        image = ImageTk.PhotoImage(data=data.read())
+        Label(root, image=image).pack()
+        root.mainloop()
 
-print (f"PFP: https://cdn.discordapp.com/embed/avatars/{modID}.png")
+if option == 2: #Current system
+    userID = int(input("""
+To find a user ID: Go to Discord Settings --> Advanced --> Turn on Developer Mode --> Right Click on User --> Click Copy User ID
+
+Please enter the user's user ID: """))
+    shiftedID = userID >> 22 #Right binary shift userID by 22 places
+    modID = shiftedID % 6 #shiftedID modulo 6
+    
+    print (f"The profile picture can be seen at: https://cdn.discordapp.com/embed/avatars/{modID}.png")
+    showImage = ""
+    while showImage.capitalize() not in ["Y", "Yes", "N", "No"]:
+        showImage = input("Would you like to see the image? [Y]es or [N]o: ")
+    if showImage.capitalize() in ["Y", "Yes"]:
+        req = Request(
+            url=f'https://cdn.discordapp.com/embed/avatars/{modID}.png', #Image URL
+            headers={'User-Agent': 'Mozilla/5.0'} #Tells Discord CDN we are using a Mozilla browser
+        )
+        #Gets the image from the URL and displays it
+        root = Tk()
+        data = urlopen(req)
+        image = ImageTk.PhotoImage(data=data.read())
+        Label(root, image=image).pack()
+        root.mainloop()
